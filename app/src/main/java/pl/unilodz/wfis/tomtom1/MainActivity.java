a7ed926869da8743573944797a0b1f47f4edb74c
michal.ms36 <michal.ms36@gmail.com>
2021-06-22 01:34:59 +0200

    Wyświetlanie mapy

--- app/src/main/java/pl/unilodz/wfis/tomtom1/MainActivity.java
+++ app/src/main/java/pl/unilodz/wfis/tomtom1/MainActivity.java
@@ -1,19 +1,69 @@
 package pl.unilodz.wfis.tomtom1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tomtom.online.sdk.common.location.LatLng;
+import com.tomtom.online.sdk.map.ApiKeyType;
+import com.tomtom.online.sdk.map.MapFragment;
+import com.tomtom.online.sdk.map.MapProperties;
+import com.tomtom.online.sdk.map.OnMapReadyCallback;
+import com.tomtom.online.sdk.map.TomtomMap;
+import com.tomtom.online.sdk.map.TomtomMapCallback;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, TomtomMapCallback.OnMapLongClickListener {
+
+    private TomtomMap tomtomMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
-        int w=0;
-        for(int i=0; i<10;i++){
-            w=w+i;
-        }
+        initTomTomServices();
+        initUIViews();
+        setupUIViewListeners();
     }
+
+    @Override
+    public void onMapReady(@NonNull final TomtomMap tomtomMap) {
+        this.tomtomMap = tomtomMap;
+        this.tomtomMap.setMyLocationEnabled(true);
+        this.tomtomMap.addOnMapLongClickListener(this);
+        this.tomtomMap.getMarkerSettings().setMarkersClustering(true);
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {}
+
+    private void initTomTomServices() {
+        Map<ApiKeyType, String> mapKeys = new HashMap<>();
+        mapKeys.put(ApiKeyType.MAPS_API_KEY, "rAT1nOItZYvJEr6XE6TpXQCkre6x4oQA");
+
+        MapProperties mapProperties = new MapProperties.Builder()
+                .keys(mapKeys)
+                .build();
+        MapFragment mapFragment = MapFragment.newInstance(mapProperties);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mapFragment, mapFragment)
+                .commit();
+        mapFragment.getAsyncMap(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        this.tomtomMap.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void initUIViews() {}
+    private void setupUIViewListeners() {}
+
 }
