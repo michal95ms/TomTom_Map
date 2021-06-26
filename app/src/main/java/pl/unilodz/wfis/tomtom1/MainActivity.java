abf08b42191a7bc8e54a563b1e0afef96eb5a587
michal.ms36 <michal.ms36@gmail.com>
2021-06-27 00:37:25 +0200

    Pasek nawigacyjny

--- app/src/main/java/pl/unilodz/wfis/tomtom1/MainActivity.java
+++ app/src/main/java/pl/unilodz/wfis/tomtom1/MainActivity.java
@@ -2,9 +2,14 @@ package pl.unilodz.wfis.tomtom1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tomtom.online.sdk.common.location.LatLng;
 import com.tomtom.online.sdk.map.ApiKeyType;
 import com.tomtom.online.sdk.map.MapFragment;
@@ -12,24 +17,75 @@ import com.tomtom.online.sdk.map.MapProperties;
 import com.tomtom.online.sdk.map.OnMapReadyCallback;
 import com.tomtom.online.sdk.map.TomtomMap;
 import com.tomtom.online.sdk.map.TomtomMapCallback;
+import com.tomtom.online.sdk.search.OnlineSearchApi;
+import com.tomtom.online.sdk.search.SearchApi;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import pl.unilodz.wfis.tomtom1.fragments.FavouriteFragment;
+import pl.unilodz.wfis.tomtom1.fragments.SearchFragment;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, TomtomMapCallback.OnMapLongClickListener {
 
     private TomtomMap tomtomMap;
+    MapFragment mapFragment;
+    FavouriteFragment favouriteFragment;
+    SearchFragment searchFragment;
+    RelativeLayout relativeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment = new MapFragment();
+        favouriteFragment = new FavouriteFragment();
+        searchFragment = new SearchFragment();
+        relativeLayout = (RelativeLayout) findViewById(R.id.fragment_container);
+
         initTomTomServices();
         initUIViews();
         setupUIViewListeners();
+
+/*
+        BottomNavigationView bottomNavigationView= findViewById(R.id.top_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MapFragment()).commit();
+        }*/
+
     }
+/*
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.ic_map:
+                            relativeLayout.setVisibility(View.INVISIBLE);
+                            selectedFragment = mapFragment;
+                            break;
+                        case R.id.ic_favourite:
+                            relativeLayout.setVisibility(View.VISIBLE);
+                            selectedFragment = favouriteFragment;
+                            break;
+                        case R.id.ic_search:
+                            relativeLayout.setVisibility(View.VISIBLE);
+                            selectedFragment = searchFragment;
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+*/
 
     @Override
     public void onMapReady(@NonNull final TomtomMap tomtomMap) {
@@ -50,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapProperties mapProperties = new MapProperties.Builder()
                 .keys(mapKeys)
                 .build();
-        MapFragment mapFragment = MapFragment.newInstance(mapProperties);
+        com.tomtom.online.sdk.map.MapFragment mapFragment = com.tomtom.online.sdk.map.MapFragment.newInstance(mapProperties);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.mapFragment, mapFragment)
@@ -69,5 +125,4 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void setupUIViewListeners() {
     }
-
 }
